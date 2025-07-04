fe4a66e8f8668c930838903d7a3e526e8871a5bc
gabriellabonette <gabriella.bonette@escola.pr.gov.br>
2025-07-04 08:28:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,14 @@
+criaCartao(
+'Programação',
+'O que é logica de programação?',
+'Lógica de programação é o conjunto de regras e técnicas usadas para desenvolver sequências lógicas, permitindo que computadores ou outros dispositivos resolvam problemas de forma eficiente.'
+);
+criaCartao(
+'Geografia',
+'Oque é U.R.S.S?',
+'URSS é a sigla para União das Repúblicas Socialistas Soviéticas, um antigo estado socialista que existiu de 1922 a 1991.'
+);
+criaCartao(
+'Matemática',
+'37% de 4000',
+'37% de 4000 é 1480.');
